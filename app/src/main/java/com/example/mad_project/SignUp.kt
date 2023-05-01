0ad0fab75729a99cb0d692cbe0ed8bbdf0bd7f94
Muditha Pradeeptha <99239289+IT21235924@users.noreply.github.com>
2023-05-02 02:25:56 +0530

    Login and SignUp pages Done

--- app/src/main/java/com/example/mad_project/SignUp.kt
+++ app/src/main/java/com/example/mad_project/SignUp.kt
@@ -3,10 +3,9 @@ package com.example.mad_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_sign_up)
     }
 }
