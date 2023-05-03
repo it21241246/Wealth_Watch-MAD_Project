586f5291f39655328dc7ebe62862a45c9bd81542
Muditha Pradeeptha <99239289+IT21235924@users.noreply.github.com>
2023-05-03 17:22:49 +0530

    database adding done

--- app/src/main/java/com/example/mad_project/Login.kt
+++ app/src/main/java/com/example/mad_project/Login.kt
@@ -1,11 +1,62 @@
 package com.example.mad_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mad_project.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            } else {
+                Toast.makeText(
+                    this,
+                    "We told you , Empty fiels are not allowed !",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    }
+
+    override fun onStart(){
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
--- app/src/main/java/com/example/mad_project/SignUp.kt
+++ app/src/main/java/com/example/mad_project/SignUp.kt
@@ -1,15 +1,22 @@
 package com.example.mad_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.mad_project.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignUp : AppCompatActivity() {
-
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,23 +25,50 @@ class SignUp : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
         binding.button.setOnClickListener {
+            val name = binding.nameEt.text.toString()
+            val phone = binding.phoneEt.text.toString()
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
 
-            if(email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
-                if (pass == confirmPass){
+            if (name.isNotEmpty() && phone.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
 
-                    firebaseAuth.createUserWithEmailAndPassword(email , pass).addOnCompleteListener{
-                        
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val userID = FirebaseAuth.getInstance().currentUser!!.uid
+                            val userMap = hashMapOf(
+                                "name" to name,
+                                "phone" to phone,
+                                "email" to email,
+                                "password" to pass,
+                                "timestamp" to FieldValue.serverTimestamp()
+                            )
+
+                            db.collection("users").document(userID).set(userMap).addOnSuccessListener {
+                                Toast.makeText(this, "Successfully Added!", Toast.LENGTH_SHORT).show()
+
+                                val intent = Intent(this, Login::class.java)
+                                startActivity(intent)
+                                
+                            }.addOnFailureListener {
+                                Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
                     }
-
-                }else{
-                    Toast.makeText(this , "Password is not matching you idiot !", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT).show()
                 }
-            }else{
-                Toast.makeText(this, "We told you , Empty fiels are not allowed !", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please fill all fields!", Toast.LENGTH_SHORT).show()
             }
         }
     }
