348db9b72953c64e9ff98e1e370e1e94cfcae49a
IT21237690 <it21237690@my.sliit.lk>
2023-05-04 23:57:09 +0530

    userid
    userid added for expenses

--- app/src/main/java/com/example/mad_project/Expenses.kt
+++ app/src/main/java/com/example/mad_project/Expenses.kt
@@ -1,10 +1,11 @@
 import com.google.firebase.Timestamp
 import java.text.SimpleDateFormat
 import java.util.*
+import com.google.firebase.auth.FirebaseAuth
 
-class Expenses(var name: String, var amount: Double, var date: Timestamp? = null, var id: String? = "") {
+class Expenses(var name: String, var amount: Double, var date: Timestamp? = null, var id: String? = "",var userid: String?) {
 
-    constructor() : this("", 0.0, null, null)
+    constructor() : this("", 0.0, null, null,null)
 
     fun getDateString(): String {
         if (date == null) {
