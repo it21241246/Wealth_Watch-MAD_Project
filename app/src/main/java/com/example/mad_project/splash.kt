24d99f7fecbbe93ffe907606bd7dd057b72e003d
Muditha Pradeeptha <99239289+IT21235924@users.noreply.github.com>
2023-05-08 19:00:50 +0530

    debug

---
+++ app/src/main/java/com/example/mad_project/splash.kt
@@ -0,0 +1,32 @@
+package com.example.mad_project
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        //Normal Handler is deprecated , so we have to change the code little bit
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 2000 is the delayed time in milliseconds.
+
+    }
+}
